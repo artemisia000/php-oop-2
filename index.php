3302ee91d1526121534fe7083b65efd49ad1ad65
artemisia000 <misiapierobon@gmail.com>
2022-01-10 16:33:21 +0100

    ereditarietà

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-Oggi pomeriggio provate ad immaginare alcune classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da ///acquistare e degli utenti che fanno shopping.Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.Eseguire poi degli output istanziando oggetti delle varie classi.
+Oggi pomeriggio provate ad immaginare alcune classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.Strutturare le classi gestendo l’ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.Eseguire poi degli output istanziando oggetti delle varie classi.
 
 <?php
 
+#classe padre principale
 class Book {
     public $title;
     public $author;
@@ -22,8 +23,30 @@ public function getSales($sale) {
 }
 }
 
+#classe figlio/eredito
+class LonelyPlanet extends Book{
+    //proprietà
+    public $lastUpdate;
+     
+    //costruttore richiamando parametri padre più quelli aggiunti nel figlio
+    public function __construct($title,$author,$price,$lastUpdate) {
+
+        parent::__construct($title,$author,$price);
+
+        $this->lastUpdate = $lastUpdate;
+    }
+
+}
+
+//instanza da padre
 $book = new Book ('CUJO','Stephen King',15);
 
 echo 'Prezzo scontato:' . $book->getSales(10);
+
+//instanza da figlio/eredito
+$Japan = new LonelyPlanet('Giappone','LonelyPlanet', 34, 2018);
+var_dump($Japan);
+
+echo "Prezzo scontato:" . $Japan->getSales(25);
 
 ?>
